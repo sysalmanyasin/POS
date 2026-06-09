c48a6edd4e0de6eafec36222079618247152c2b1
sysalmanyasin <sy.salmanyasin@gmail.com>
2026-06-09 12:07:19 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Version: pharmapos-cache-v12.4 (Added syncHub.js + auditLog.js to offline cache)
  */
 
-const CACHE_NAME = 'pharmapos-cache-v13.2';
+const CACHE_NAME = 'pharmapos-cache-v13.3';
 
 // Explicit structural cache list to guarantee the system works offline instantly on day one
 const CORE_ASSETS = [
